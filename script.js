45ff0005d2f60d61ed4b36dc8c5c404feb6a10ab
souta <kansouta1125@icloud.com>
2025-04-22 15:59:22 +0900

    update

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 const el = window;
+let prevX = 0;
+let prevY = 0;
+
+// タッチ開始
 el.addEventListener("touchstart", (e) => {
-  console.log(e.targetTouches[0], e.targetTouches[1]);
+  const touches = e.touches[0];
+  prevX = touches.screenX;
+  prevY = touches.screenY;
+})
+
+// 移動
+el.addEventListener("touchmove", (e) => {
+  const touches = e.touches[0];
+  
+  const x = touches.screenX;
+  const y = touches.screenY;
+
+  const dx = x - prevX;
+  const dy = y - prevY;
+
+  prevX = x;
+  prevY = y;
+
+  console.log(dx, dy)
 })
